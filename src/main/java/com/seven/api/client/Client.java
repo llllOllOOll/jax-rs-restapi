248f592b20ef70c7966f856c0033764d92678edc
seven <7b37b3@gmail.com>
2024-08-08 11:15:58 -0300

    feat: add Client class

---
+++ src/main/java/com/seven/api/client/Client.java
@@ -0,0 +1,31 @@
+package com.seven.api.client;
+
+public class Client {
+  private Long id;
+  private String email;
+  private String phone;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+}
