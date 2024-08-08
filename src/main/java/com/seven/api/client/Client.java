567113cc047d2b2402882fd9bc7627ac64ed5a4e
seven <7b37b3@gmail.com>
2024-08-08 11:22:12 -0300

    fix: Constructs and attibuts

--- src/main/java/com/seven/api/client/Client.java
+++ src/main/java/com/seven/api/client/Client.java
@@ -2,11 +2,30 @@ package com.seven.api.client;
 
 public class Client {
   private Long id;
+  private String name;
   private String email;
   private String phone;
 
+  public Client(Long id, String name, String email, String phone) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  };
+
+  public Client() {
+  };
+
   public Long getId() {
     return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   public void setId(Long id) {
